bd93e3824d5133a554035b184322f7d358474b2b
yoosc89 <yoosc89@gmail.com>
2023-10-04 14:09:07 +0900

    calendar 컨트롤러 라우터 추가

--- src/controllers/calendar.js
+++ src/controllers/calendar.js
@@ -1,11 +1,60 @@
-import { serviceCalendarByFindAll, } from '../services/calendar.js';
+import {
+    deleteById,
+    myCalendarList as myCalList,
+    serviceCalendarByFindAll, updateById as calUpdateById,
+    updateCalendarIndexNo as calIndexChange,
+} from '../services/calendar.js';
+
+export const member = ({member}) => {
+    return {
+        memberCode: member['MEMBER_CODE'],
+        department: member['DEPT_CODE'],
+    };
+}
 
 export const calendarByFindAll = async (req, res ,next) => {
 
-    const id = req.member['MEMBER_CODE'];
-    const department = req.member['DEPT_CODE'];
+    const {memberCode, department} = member({member: req.member});
 
-    const calendarList = await serviceCalendarByFindAll({id: id, department: department});
-    res.status(200).json({stauts: 200, message: 'success', data: calendarList});
+    const calendarList = await serviceCalendarByFindAll({id: memberCode, department: department});
+    return res.status(200).json({stauts: 200, message: 'success', data: calendarList});
 }
+
+export const myCalendarList = async (req, res, next) => {
+    const {memberCode, department} = member({member: req.member});
+
+    const result = await myCalList({
+        memberCode: memberCode, departmentCode: department
+    })
+
+    return res.stauts(result.status).json(result);
+}
+
+export const updateByCalendar = async (req, res, next) => {
+    const {memberCode} = member({member: req.member});
+    const result = await calUpdateById({calendar: req.calendar})
+    return res.stauts(result.status).json(result);
+}
+
+export const updateCalendarIndexNo = async (req, res, next) => {
+
+    const {memberCode} = member({member: req.member});
+
+    const result = await calIndexChange({info: req.info, memberCode: memberCode});
+
+    return res.stauts(result.status).json(result);
+}
+
+export const deleteByCalendar = async (req, res, next) => {
+
+    const calendarCode = req.query.calendarCode;
+    const {memberCode} = member({member: req.member});
+
+    const result = await deleteById({calendarCode: calendarCode, memberCode: memberCode})
+
+    return res.stauts(result.status).json(result);
+}
+
+
+
 
--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -1,8 +1,21 @@
-import { calendarByFindAll } from '../controllers/calendar.js';
+import {
+    calendarByFindAll,
+    deleteByCalendar,
+    myCalendarList,
+    updateByCalendar,
+    updateCalendarIndexNo,
+} from '../controllers/calendar.js';
 import express from 'express';
 const router = express();
 
 
-router.get('/', calendarByFindAll);
+router.get('/list', calendarByFindAll);
+router.get('/mylist', myCalendarList);
+// router.get('/ergist', sav);
+router.get('/update', updateByCalendar);
+// router.get('/updateDafault', updated);
+router.get('/changeIndexNo', updateCalendarIndexNo);
+router.get('/delete/:calendarCode', deleteByCalendar);
+// router.get('/delete/openCalendarList', find);
 
 export default router;
